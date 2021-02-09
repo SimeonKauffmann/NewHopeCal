7c8a58015dc3dbf876cf039664c938bdf8c7946c
Simeon Kauffmann <simeon.kauffmann@gmail.com>
2021-02-09 15:17:41 +0100

    started with week and sidebar components

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,13 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app{
+    background-color: #608B96;
   }
-}
+  p{
+    font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  }
+  h1, h2, h3, h4{
+    font-family: 'Cabin', 'Times New Roman', Times, serif ;
+  }
 </style>
